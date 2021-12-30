3f5b5e9671fda23eb7220d44c0350aa4579707ab
Nathalia-Urenha <naty.urenha@gmail.com>
2021-12-29 21:53:25 -0300

    Repository List

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,21 +1,26 @@
 import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss';
+import { useState, useEffect } from 'react';
 
-const repository = {
-    name: 'GitHub Explorer',
-    descripstion: "Projeto em React", 
-    link: "https://github.com/Nathalia-Urenha/github-explorer-rocketseat"
-}
 
 export function RepositoryList(){
+
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(()=> {
+        fetch('https://api.github.com/users/Nathalia-Urenha/repos')
+        .then(response => response.json())
+        .then(data => setRepositories(data))
+    }, []);
+
     return(
         <section className="repository-list">
             <h1>Lista de Repositórios</h1>
             <ul>
-              <RepositoryItem repository={repository}/>
-              <RepositoryItem repository={repository}/>
-              <RepositoryItem repository={repository}/>
-              <RepositoryItem repository={repository}/>
+                {repositories.map(repository => {
+                    return <RepositoryItem key={repository.name} repository={repository} />
+                })}
+            
             </ul>
         </section>
     )
